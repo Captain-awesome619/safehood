ebe7cebc0e177e5d0c87d96e62e2a6aa61f32684
Captain-awesome19 <ogunsolatoluwalase@gmail.com>
2025-05-02 08:38:31 +0100

    chat

--- src/app/components/Chats.tsx
+++ src/app/components/Chats.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='flex items-center justify-center'>
+ <h3 className='lgtext-[24px] text-[17px] font-[600] text-primary1'>Feature coming Soon</h3>
+    </div>
   )
 }
 
